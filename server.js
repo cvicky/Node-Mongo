d91d64eca74e0b912b072ff587e209022e56e0d0
Vicky Chen <c.vicky1213@gmail.com>
2017-04-18 18:19:43 -0400

    can now search by id

--- server.js
+++ server.js
@@ -1,14 +1,39 @@
-const http = require('http');
+'use strict';
 
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors'); //front end is going to be served from 8080
+let contacts = require('./data');
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(cors());
+
+app.get('/api/contacts', (request, response) =>{
+	if(!contacts){
+		response.status(404).json({message: 'No contacts found'});
+	}
+	response.json(contacts);
+})
+
+app.get('/api/contacts/:id', (request, response) =>{
+	const requestId = request.params.id;
+
+	let contact = contacts.filter(contact => {
+		return contact.id == requestId;
+	});
+
+	response.json(contact[0]);
+})
 const hostname = 'localhost';
-const port = 3001;
+const port = 3003; // this API is served from 3003
 
-const server =http.createServer((request, response)=> {
-	response.statusCode = 200;
-	response.setHeader('Content-Type', 'text/html');
-	response.end('<h1>Hello World!</h1>');
-});
+// const server =http.createServer((request, response)=> {
+// 	response.statusCode = 200;
+// 	response.setHeader('Content-Type', 'text/html');
+// 	response.end('<h1>Hello World!</h1>');
+// });
 
-server.listen(port, hostname, () =>{
+app.listen(port, hostname, () =>{
 	console.log(`Server is running at http://${hostname}:${port}`);
 })
